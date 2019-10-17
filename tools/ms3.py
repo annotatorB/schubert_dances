2ec5d1a99421b7d0479eef0c753b3abf2fe8e17d
laserjeyes <johannes.hentschel@epfl.ch>
2019-10-17 19:12:20 +0200

    Added first attempt at section parsing

--- tools/ms3.py
+++ tools/ms3.py
@@ -40,6 +40,7 @@ Use logging.info('Message') for messages that users would want to see in everyda
 #Internal libraries
 ###################
 import os, re, argparse, logging
+from collections import defaultdict
 
 
 ###################
@@ -54,11 +55,42 @@ import numpy as np
 #########
 NEWEST_MUSESCORE = '3.2.3'
 
+
+################################################################################
+#                            HELPER FUNCTIONS
+################################################################################
+def search_in_list_of_tuples(L, pos, search, add=0):
+    """ Returns a list of indices.
+
+    Parameters
+    ----------
+    L : :obj:`list` of :obj:`tuple`
+        List of tuples in which you want elements with value `search`.
+    pos : :obj:`int`
+        In which position of the tuples to search.
+    search : :obj:`object`
+        What to look for.
+    add : :obj:`int`, opt
+        How much you want to add to each index value.
+    """
+    return [i+add for i, item in enumerate(L) if search in item[pos]]
+
+
+
+
+
+
+
+
 ################################################################################
 #                             SCORE CLASS
 ################################################################################
-class score(object):
+class Score(object):
     """ Parser for MuseScore3 MSCX files.
+
+    NOTE: Measure count `mc` refers to the `mc`th measure node, whereas measure
+    number `mn` refers to the `mn`th measure in the score. The latter can consist
+    of several measure nodes and can be split across sections.
 
     Attributes
     ----------
@@ -68,11 +100,18 @@ class score(object):
         Absolute or relative path to the MSCX file you want to parse.
     filename : :obj:`str`
         Filename of the parsed file.
-    measure_nodes = :obj:`dict` of :obj:`dict` of bs4.BeautifulSoup
+    last_node : :obj:`int`
+        Count of the score's last measure node.
+    measure_nodes : :obj:`dict` of :obj:`dict` of bs4.BeautifulSoup
         Keys of the first dict are staff IDs, keys of each inner dict are incremental
         measure counts (NOT measure numbers) and values are XML nodes.
     score : bs4.BeautifulSoup
         The complete XML structure of the parsed MSCX file.
+    section_breaks : :obj:`dict`
+        Keys are the counts of the measures that have a section break, values are
+        lists of the breaking elements {startRepeat, endRepeat, BarLine}
+    sections : :obj:`dict` of `Section`
+        The sections of this score.
     staff_nodes : :obj:`dict` of bs4.BeautifulSoup
         Keys are staff IDs starting with 1, values are XML nodes.
 
@@ -85,6 +124,8 @@ class score(object):
         self.dir, self.filename = os.path.split(os.path.abspath(file))
         self.staff_nodes = {}
         self.measure_nodes = {}
+        self.section_breaks = defaultdict(list)
+        self.sections = {}
 
         # Load file
         with open(self.file, 'r') as file:
@@ -104,15 +145,127 @@ class score(object):
             logging.debug(f"Stored staff with ID {staff_id}.")
 
         # Extract measures
+        break_tags = ['startRepeat', 'endRepeat', 'BarLine', 'Volta']
+
         for staff_id, staff in self.staff_nodes.items():
             for i, measure in enumerate(staff.find_all('Measure')):
                 self.measure_nodes[staff_id][i] = measure
                 logging.debug(f"Stored the {i}th measure of staff {staff_id}.")
 
+                # Detect section breaks
+                breaks = [tag.name for tag in measure.find_all(break_tags)]
+                if len(breaks) > 0:
+                    self.section_breaks[i].extend(breaks)
+                    logging.debug(f"Found section break(s) {breaks} in the {i}th measure of staff {staff_id}.")
 
-S = score('./scores/980/D980walzer02.mscx')
-S.measure_nodes[1][0]
+        # Store last measure, presupposing that the first staff spreads throughout the piece.
+        self.last_node =  max(self.measure_nodes[1].keys())
 
+        self.parse_section_breaks()
+
+
+
+    def parse_section_breaks(self):
+
+        self.section_breaks[0].append('start')
+        self.section_breaks[self.last_node].append('end')
+        breaks = sorted(self.section_breaks.items())
+
+        section_start = 0
+        for splitpoint in search_in_list_of_tuples(breaks, 1, 'endRepeat', 1):
+            if splitpoint == len(breaks):
+                section = breaks[section_start:]
+            else:
+                voltas = search_in_list_of_tuples(breaks[:splitpoint], 1, 'Volta')
+                if len(voltas) > 0:
+                    while ('Volta' in breaks[splitpoint][1]) and (splitpoint < len(breaks)):
+                        splitpoint += 1
+                section = breaks[section_start:splitpoint]
+
+            S = Section()
+            next_mc = S.init(section, self, repeated=True)
+            self.add_section(S)
+            if next_mc <= self.last_node and breaks[splitpoint][0] != next_mc:
+                breaks.insert(splitpoint, (next_mc, ['after_volta']))
+            section_start = splitpoint
+
+
+
+    def add_section(self, section):
+        k = self.sections.keys()
+        if len(k) > 0:
+            next_key = max(k) + 1
+        else:
+            next_key = 0
+        section.index = next_key
+        self.sections[next_key] = section
+
+S = Score('../scores/testscore.mscx')
+S.sections
+
+################################################################################
+#                             SECTION CLASS
+################################################################################
+class Section(object):
+    """ Holds the properties of a section.
+
+    Attributes
+    ----------
+    first_measure, last_measure : :obj:`int`
+        Measure counts of the section's first and last measure nodes.
+    first_break, last_break : :obj:`str`
+        What causes the section breaks at either side.
+    index : :obj:`int`
+        Index (running number) of this section.
+    subsections : :obj:`list` of :obj:`int`
+        Indices of smaller sections that are included in this one.
+    voltas : :obj:`list` of :obj:`range`
+        Ranges of voltas. Default: empty list
+    repeated : :opj:`bool`
+        Whether or not this section is repeated.
+    """
+
+    def __init__(self):
+        self.first_measure, self.last_measure, self.index = 0, 0, 0
+        self.first_break, self.last_break = '', ''
+        self.subsections, self.voltas = [], []
+        self.repeated = False
+
+    def init(self, breaks, parent_score, repeated=False):
+
+        if repeated:
+            self.repeated = True
+
+        self.first_measure, self.first_break = breaks[0]
+        voltas = search_in_list_of_tuples(breaks, 1, 'Volta')
+        if len(voltas) == 0:
+            self.last_measure, self.last_break   = breaks[-1]
+            following = self.last_measure + 1
+        else:
+            for volta in voltas:
+                mc = breaks[volta][0]
+                node = parent_score.measure_nodes[1][mc].find('Volta')
+                length = int(node.find_next('next').location.measures.string)
+                following = mc + length
+                self.voltas.append(range(mc, following))
+            self.last_measure = following - 1
+            self.last_break = breaks[-1][1]
+
+        self.first_break = list(set(self.first_break))
+        self.last_break = list(set(self.last_break))
+        return following
+
+
+
+    def __repr__(self):
+        return f"{'Repeated s' if self.repeated else 'S'}ection from node {self.first_measure} ({', '.join(self.first_break)}) to node {self.last_measure} ({', '.join(self.last_break)}), {'with ' + str(len(self.voltas)) if len(self.voltas) > 0 else 'without'} voltas."
+
+
+
+
+################################################################################
+#                           COMMANDLINE USAGE
+################################################################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
