14ef0f18349ee66c3d647eea2c61a912f89c2127
laserjeyes <jeyes@web.de>
2019-12-17 18:09:01 +0100

    Worked on introduction

--- jh.py
+++ jh.py
@@ -1,1 +1,15 @@
 import pandas as pd
+from fractions import Fraction as frac
+
+def read_note_list(file, index_col=[0,1], converters={}, dtypes={}):
+    conv = {'onset':frac,
+                'duration':frac,
+                'nominal_duration':frac,
+                'scalar':frac}
+    types = {'tied': 'Int64',
+             'volta': 'Int64'}
+    types.update(dtypes)
+    conv.update(converters)
+    return pd.read_csv(file, sep='\t', index_col=index_col,
+                                dtype=types,
+                                converters=conv)
