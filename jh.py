28dd678ae6123b1e875a0b571f72e03a16139687
laserjeyes <jeyes@web.de>
2019-12-19 12:03:33 +0100

    jh update

--- jh.py
+++ jh.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from collections import defaultdict
 from fractions import Fraction as frac
 
@@ -35,6 +36,10 @@ SYLLABLES.update({ # https://www.epos.uni-osnabrueck.de/books/l/lehs007/pages/18
 (frac(7, 32), frac(1, 32)): 'ri',
 })
 
+TPC_INT_NUM = [4, 1, 5, 2, 6, 3, 7]
+TPC_INT_QUA = {0: ['P', 'P', 'P', 'M', 'M', 'M', 'M'],
+               -1:['D', 'D', 'D', 'm', 'm', 'm', 'm']}
+
 class defaultfunctiondict(defaultdict):
     """This class lets you create a defaultdict which applies a custom function on missing keys.
 
@@ -55,6 +60,37 @@ TIMESIG_BEAT.update({'4/8':  '1/4',})
 
 # def get_onset_pattern(note_list):
 #     return pd.Series({'pattern': tuple(sorted(set(note_list.onset.values))), 'end': frac(note_list.timesig.values[0])})
+
+
+
+def add_chord_boundaries(chord_list, measure_list, next_ids='chord_id', multiple_pieces=False):
+    """Single piece!"""
+    if multiple_pieces:
+        ix = chord_list.index.get_level_values(0)[0]
+        chord_list = chord_list.droplevel(0)
+        measure_list = measure_list.loc[ix]
+    chord_list = chord_list.copy()
+    ix = chord_list.index
+    shifted = chord_list.reset_index()[['mc', 'onset', next_ids]].shift(-1).astype({'mc': 'Int64', next_ids:'Int64'})
+    shifted.index = ix
+    chord_list[['mc_next', 'onset_next', 'next_'+next_ids]] = shifted
+    pos_cols = [chord_list.columns.get_loc('mc_next'), chord_list.columns.get_loc('onset_next')]
+    chord_list.iloc[-1, pos_cols] = [measure_list.index.get_level_values('mc').max() + 1, 0]
+    chord_list['chord_length'] = chord_list.apply(lambda r: get_onset_distance((r.mc, r.onset), (r.mc_next, r.onset_next), measure_list['act_dur']), axis=1)
+    return chord_list
+
+
+
+def all_chord_notes(chord_list, note_list, by='chord_id', multiple_pieces=False):
+    """
+    Parameters
+    ----------
+    """
+    if multiple_pieces:
+        id = chord_list.index.get_level_values(0)[0]
+        chord_list = chord_list.droplevel(0)
+        note_list = note_list.loc[id]
+    return chord_list.groupby(by=by).apply(chord_notes, note_list)
 
 
 
@@ -80,24 +116,33 @@ def apply_function(f, val, *args, **kwargs):
 
 
 
-def create_os_features(onset_patterns, measure_counts):
-    """Compute the fraction of every occurring onset pattern per piece.
+def apply_to_pieces(f, df, *args, **kwargs):
+    """ Applies a function `f` which works on single pieces only to a dataframe of
+    concatenated pieces distinguished by the first MultiIndex level.
+    """
+    return df.groupby(level=0).apply(f, *args, **kwargs)
 
+
+
+def chord_notes(df, notes, **kwargs):
+    """ Helper function for all_chord_notes()
     Parameters
     ----------
-    onset_patterns : :obj:`pandas.Series`
-        The measures with th eonset patterns you want to count.
-    measure_counts : :obj:`pandas.Series`
-        For every id, the amount of measures in the piece.
+    df : :obj:`pandas.DataFrame`:
+        A DataFrame with chord information, as created by a groupby('chord_id') on
+        a chord list that has been treated with add_chord_boundaries(). Only the
+        first row of `df` is being used.
+    notes : :obj:`pandas.DataFrame`:
+        Note list from which the harmony block is extracted
     """
-    def os_fraction(patterns):
-        id = patterns.index.levels[0][0]
-        counts = patterns.value_counts()
-        n = measure_counts[id]
-        return counts / n
-    res = pd.DataFrame(onset_patterns.groupby('id').apply(os_fraction)).unstack()
-    res = res.droplevel(0, axis=1)
-    return res
+    S = df.iloc[0]
+    # try:
+    notes = get_block(notes, (S.mc, S.onset), (S.mc_next, S.onset_next), cut_durations=True)
+    # except:
+    #     print(S)
+    if 'rel_label' in S:
+        notes.chords = S.rel_label
+    return notes.sort_values(['mc', 'onset', 'midi'])
 
 
 
@@ -138,6 +183,152 @@ def compute_beat_column(note_list, measure_list, inplace=False):
 
 
 
+def create_os_features(onset_patterns, measure_counts):
+    """Compute the fraction of every occurring onset pattern per piece.
+
+    Parameters
+    ----------
+    onset_patterns : :obj:`pandas.Series`
+        The measures with th eonset patterns you want to count.
+    measure_counts : :obj:`pandas.Series`
+        For every id, the amount of measures in the piece.
+    """
+    def os_fraction(patterns):
+        id = patterns.index.levels[0][0]
+        counts = patterns.value_counts()
+        n = measure_counts[id]
+        return counts / n
+    res = pd.DataFrame(onset_patterns.groupby('id').apply(os_fraction)).unstack()
+    res = res.droplevel(0, axis=1)
+    return res
+
+
+
+def get_block(note_list, start, end, cut_durations=False, staff=None, merge_ties=True):
+    """ Whereas get_slice() gets sounding notes at a point, get_block() retrieves
+    sounding notes within a range.
+    The function adds the column `overlapping` whose values follow the same logic as `tied`:
+        NaN for events that lie entirely within the block.
+        -1  for events crossing `start` and ending within the block.
+        1   for events crossing `end`.
+        0   for events that start before and end after the block.
+
+    Parameters
+    ----------
+    note_list : :obj:`pandas.DataFrame`
+        Note list from which to retrieve the block.
+    start, end : :obj:`tuple` of (:obj:int, numerical)
+        Pass (mc, onset) tuples. `end` is exclusive
+    cut_durations : :obj:`bool`, optional
+        Set to True if the original note durations should be cut at the block boundaries.
+    staff : :obj:`int`, optional
+        Return block from this staff only.
+    merge_ties : :obj:`bool`, optional
+        By default, tied notes are merged so that they do not appear as two different onsets.
+    """
+    a_mc, a_onset = start
+    b_mc, b_onset = end
+    a_mc, b_mc = int(a_mc), int(b_mc)
+    assert a_mc <= b_mc, "Start MC needs to be at most end MC."
+    a_onset, b_onset = frac(a_onset), frac(b_onset)
+    if a_mc == b_mc:
+        assert a_onset <= b_onset, "Start onset needs to be at most end onset."
+
+    res = note_list[(note_list.mc >= a_mc) & (note_list.mc <= b_mc)]
+
+    if staff is not None:
+        res = res[res.staff == staff]
+
+    in_a = (res.mc == a_mc)
+    in_b = (res.mc == b_mc)
+    endpoint = res.onset + res.duration
+    crossing_left = in_a & (res.onset < a_onset) & (a_onset < endpoint)
+    on_onset = in_a & (res.onset == a_onset)
+    crossing_right = in_b & (endpoint > b_onset)
+
+    if a_mc == b_mc:
+        in_between = in_a & (res.onset >= a_onset) & (res.onset < b_onset)
+    else:
+        onset_in_a = in_a & (res.onset >= a_onset)
+        onset_in_b = in_b & (res.onset < b_onset)
+        in_between = onset_in_a | onset_in_b
+    if a_mc + 1 < b_mc:
+        in_between = in_between | ((res.mc > a_mc) & (res.mc < b_mc))
+
+
+    res = res[crossing_left | in_between].copy()
+    res['overlapping'] = pd.Series([np.nan]*len(res.index), index=res.index, dtype='Int64')
+
+    start_tie = lambda S: S.replace({np.nan:  1, -1:  0, 0: 0, 1: 1})
+    end_tie   = lambda S: S.replace({np.nan: -1, -1: -1, 0: 0, 1: 0})
+
+    if crossing_left.any():
+        res.loc[crossing_left, 'overlapping'] = end_tie(res.loc[crossing_left, 'overlapping'])
+
+    if crossing_right.any():
+        res.loc[crossing_right, 'overlapping'] = start_tie(res.loc[crossing_right, 'overlapping'])
+
+    if res.tied.notna().any():
+        tied_from_left = on_onset & res.tied.isin([-1, 0])
+        if tied_from_left.any():
+            res.loc[tied_from_left, 'overlapping'] = end_tie(res.loc[tied_from_left, 'overlapping'])
+        tied_to_right = in_b & (endpoint == b_onset) & res.tied.isin([0, 1])
+        if tied_to_right.any():
+            res.loc[tied_to_right, 'overlapping'] = start_tie(res.loc[tied_to_right, 'overlapping'])
+
+
+    if cut_durations:
+        if crossing_left.any():
+            res.loc[crossing_left, 'duration'] = res.loc[crossing_left, 'duration'] - a_onset + res.loc[crossing_left, 'onset']
+            res.loc[crossing_left, ['mc', 'onset']] = [a_mc, a_onset]
+        if crossing_right.any():
+            res.loc[crossing_right, 'duration'] = b_onset - res.loc[crossing_right, 'onset']
+
+    if merge_ties & res.tied.any():
+        merged, changes = merge_tied_notes(res, return_changed=True)
+        if len(changes) > 0:
+            new = [ix for ix, index_list in changes.items() if len(index_list) > 0 and res.at[index_list[-1], 'overlapping'] in [0,1] ]
+            tie_over = merged.index.isin(new)
+            merged.loc[tie_over] = start_tie(merged.loc[tie_over])
+            res = merged
+
+    return res
+
+
+
+def get_onset_distance(a, b, lengths=None):
+    """ Get distance in fractions of whole notes.
+    Parameters
+    ----------
+    a, b : :obj:`tuple`
+        (mc, onset) `(int, Fraction)`
+    timesig : :obj:str or number, optional
+        Only needed if onsets are in different measures
+    lengths : :obj:`dict` or :obj:`pandas.Series`
+        For every measure count, the actual length (`act_dur`) in quarter beats.
+        Single level index!
+    """
+    mc_a, os_a = a
+    mc_b, os_b = b
+    mc_a = int(mc_a)
+    mc_b = int(mc_b)
+    os_a = frac(os_a)
+    os_b = frac(os_b)
+    if mc_a == mc_b:
+        return os_b - os_a
+    else:
+        assert lengths is not None, "If the onsets are in different measures, you need to pass the measure lengths."
+        if mc_b < mc_a:
+            mc_a, mc_b = mc_b, mc_a
+            swapped = True
+        else:
+            swapped = False
+        l1 = lengths[mc_a] - os_a
+        inter_mc = sum(lengths[mc] for mc in range(mc_a + 1, mc_b))
+        return -frac(l1 + inter_mc + os_b) if swapped else frac(l1 + inter_mc + os_b)
+
+
+
 def get_pattern_list(onset_patterns, n_most_frequent=None, occurring_in_min=None, normalize=False, round=3):
     """Summarize all patterns occurring in at least `occurring_in_min` pieces."""
     pattern_list = pd.DataFrame(onset_patterns.value_counts(), columns=['counts'])
@@ -158,6 +349,52 @@ def get_pattern_list(onset_patterns, n_most_frequent=None, occurring_in_min=None
         res.counts = res.counts / pattern_list.counts.sum()
         res.n_pieces = res.n_pieces / count_pieces(onset_patterns)
     return res.round(round)
+
+
+
+def merge_tied_notes(df, return_changed=False):
+    """ In a note list, merge tied notes to single events with accumulated durations.
+    Input dataframe needs columns ['duration', 'tied', 'midi', 'staff', 'voice']
+    """
+    df = df.copy()
+    notna = df[df.tied.notna()]
+    starts = notna[notna.tied == 1]
+    drops = []
+
+    def merge(i, midi, staff, voice):
+        """Looks for the ending(s) and recursively accumulates."""
+        dur = 0
+        ixs = []
+        if i == len(notna):
+            return dur, ixs
+        else:
+            end = notna.iloc[i]
+        while end.tied == 1 or end.midi != midi\
+                            or end.staff != staff:
+                           #or end.voice != voice:  <-- caused errors
+           i += 1
+           if i == len(notna):
+               return dur, ixs
+           else:
+               end = notna.iloc[i]
+        dur += end.duration
+        ixs.append(end.name)
+        if end.tied == 0:
+            d, i = merge(i+1, midi, staff, voice)
+            dur += d
+            ixs.extend(i)
+        return dur, ixs
+
+
+    for ix, r in starts.iterrows():
+        add_dur, ixs = merge(notna.index.get_loc(ix)+1, r.midi, r.staff, r.voice)
+        df.loc[ix, 'duration'] += add_dur
+        drops.append(ixs)
+    df.drop([e for l in drops for e in l], inplace=True)
+    if return_changed:
+        return df, {k: v for k, v in zip(starts.index.to_list(), drops)}
+    else:
+        return df
 
 
 
@@ -240,6 +477,30 @@ def transpose_to_C(note_list, measure_list=None):
                                          .apply(lambda x: 6 if x > 0 else -6)                           # If the key signature is F#, shift downwards, if it's Gb, shift upwards
     note_list_transposed.midi -= midi_transposition
     return note_list_transposed
+
+
+
+def tpc2int(tpc):
+    """Return interval name of a tonal pitch class where
+       0 = 'P1', -1 = 'P4', -2 = 'm7', 4 = 'M3' etc.
+    """
+    if tpc.__class__ == float and isnan(tpc):
+        return tpc
+    try:
+        tpc = int(tpc)
+    except:
+        return apply_function(tpc2int, tpc)
+    tpc += 1
+    pos = tpc % 7
+    int_num = TPC_INT_NUM[pos]
+    qual_region = tpc // 7
+    if qual_region in TPC_INT_QUA:
+        int_qual = TPC_INT_QUA[qual_region][pos]
+    elif qual_region < 0:
+        int_qual = (abs(qual_region) - 1) * 'D'
+    else:
+        int_qual = qual_region * 'A'
+    return f"{int_qual}{int_num}"
 
 
 
